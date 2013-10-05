b307d39e7d4b34e7c1c7da20ab5d26fd7c16855a
Matt Hawkins <matthawkinsuk@gmail.com>
2013-10-05 21:07:56 +0100

    Initial commit

---
+++ pi_lite_custom_1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#--------------------------------------
+#
+# Pi-Lite Custom Example #1
+#
+# Author : Matt Hawkins
+# Date   : 05/10/2013
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
+
+import serial
+import time
+
+# Define message complete with
+# carriage return at the end
+
+sprite1 = '000000000000000000000011110000100010111001001100100101010010101010010101100100101111001001000100010000011110000000000000000000'
+sprite2 = '000000000000000000000000000000011111011111111111111111110011111110011111111111111011111111000011111000000000000000000000000000'
+
+# Configure Pi serial port
+s = serial.Serial()
+s.baudrate = 9600
+s.timeout = 0
+s.port = "/dev/ttyAMA0"
+
+try:
+    # Open serial port
+    s.open()
+except serial.SerialException, e:
+    # There was an error
+    sys.stderr.write("could not open port %r: %s\n" % (port, e))
+    sys.exit(1)
+
+print "Serial port ready"    
+
+# Clear display
+s.write("$$$ALL,OFF\r")  
+    
+# Send Sprite 1 to the Pi-Lite
+print "Sprite 1"
+s.write('$$$F' + sprite1 + '\r')  
+
+# Short delay
+time.sleep(2)
+
+# Send Sprite 2 to the Pi-Lite
+print "Sprite 1"
+s.write('$$$F' + sprite2 + '\r')  
+
+# Short delay
+time.sleep(2)
+
+print "Good bye"
---
+++ pi_lite_custom_2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#--------------------------------------
+#
+# Pi-Lite Custom Example #1
+#
+# Author : Matt Hawkins
+# Date   : 05/10/2013
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
+
+import serial
+import time
+
+# Define message complete with
+# carriage return at the end
+
+paddlock = ['000000000000000000000000000000011111011111111111111111110011111110011111111111111011111111000011111000000000000000000000000000',
+            '011100000111100000110000000110011111111111111011111111000011111000011111000011111000011111000011111000000000000000000000000000']
+
+descripton = ['Locked','Unlocked']
+            
+# Configure Pi serial port
+s = serial.Serial()
+s.baudrate = 9600
+s.timeout = 0
+s.port = "/dev/ttyAMA0"
+
+try:
+    # Open serial port
+    s.open()
+except serial.SerialException, e:
+    # There was an error
+    sys.stderr.write("could not open port %r: %s\n" % (port, e))
+    sys.exit(1)
+
+print "Serial port ready"    
+
+# Clear display
+s.write("$$$ALL,OFF\r")  
+
+loop = True
+
+frame = 0
+
+while loop:
+
+    inp = raw_input("Press [Enter] to toggle paddlock:")
+
+    if inp.lower()!="x":
+      s.write('$$$F' + paddlock[frame] + '\r')  
+      print description[frame]
+      frame = 1 - frame
+      time.sleep(2)
+    else:
+      loop = False
+
+print "Good bye"
